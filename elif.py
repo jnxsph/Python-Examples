1e6fe0de5b5ae30b5060b308a668d021c6637d1a
jnxsph <89296052+jnxsph@users.noreply.github.com>
2021-10-24 15:53:34 +0800

    Create elif.py
    Using of elif

---
+++ elif.py
@@ -0,0 +1,11 @@
+answer = input ("What is your age?")
+age = int (answer)
+
+if age < 5:
+    print("Watch Tom and Jerry.")
+elif age < 15:
+    print("Read comic books.")
+elif age < 21:
+    print("Watch Family Movie.")
+else:
+    print("Do whatever you want.")

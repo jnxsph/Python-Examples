b5d4dfc4a044fca16c39c0066feae6baad2f006f
jnxsph <89296052+jnxsph@users.noreply.github.com>
2021-10-24 21:56:59 +0800

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,99 @@
+# defining or declaring a function
+def brush_teeth():
+    print("Take the toothbrush.")
+    print("Put toothpaste on the toothbrush.")
+    print("Clean teeth")
+    print("Wash the toothbrush.")
+    print("Rinse mouth.")
+    
+    
+brush_teeth()
+############################################################################
+
+# providing information to a function
+# defining the parameter
+def five_times(num):
+    print(num*5)
+
+
+# using the parameter
+five_times(7)
+five_times(3)
+
+x = 18
+five_times(x)
+############################################################################
+
+# defining the parameter
+def add_10(num):
+    result = num + 10
+    print(result)
+    
+   
+# using the parameter    
+add_10(32)
+############################################################################
+
+# defining multiple parameter
+def add_numbers(first, second):
+    sum = first + second
+    print(sum)
+
+
+# using multiple parameter
+add_numbers(10, 2)
+############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

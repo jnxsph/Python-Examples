1cd9137fe6631b4d76288ed34ad216f0922d1c54
jnxsph <89296052+jnxsph@users.noreply.github.com>
2021-10-28 01:02:27 +0800

    Update While loop.py

--- While loop.py
+++ While loop.py
@@ -3,6 +3,8 @@ count = 0
 while count < 5:
     print(count)
     count = count + 1
+    
+#############################################################################################    
 
 # writing a while loop
 count = 5
@@ -10,6 +12,8 @@ while count > 0:
     print(count)
     count = count - 1
 
+#############################################################################################
+    
 # sum of all the numbers from 1 to 10
 i = 1
 sum = 0
@@ -17,7 +21,9 @@ while i <= 10:
     sum = sum + i
     i = i + 1
 print(sum)    
-    
+
+#############################################################################################
+
 # getting the sum of even numbers from 1 to 10
 i = 1
 sum = 0
@@ -26,6 +32,8 @@ while i <= 10:
         sum = sum + i
     i = i + 1
 print(sum)
+
+#############################################################################################
 
 # getting the sum of odd numbers from 1 to 50
 i = 1
@@ -36,6 +44,8 @@ while i <= 50:
     i = i + 1
 print(sum)
 
+#############################################################################################
+
 # breaking while loop - will break when the value becomes 5
 count = 0
 
@@ -44,6 +54,8 @@ while count < 15:
         break
     print(count)
     count += 1
+
+#############################################################################################
 
 # skipping even numbers in while loop
 count = 0
